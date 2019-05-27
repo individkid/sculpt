f27ce50b59b77a1bb8e947641a5229c70e376bc5
Paul Coelho <pcoelho@Pauls-Mac-Pro.local>
2019-05-26 21:45:44 -0700

    numeric symbolic topology

--- Main.hs
+++ Main.hs
@@ -1,3 +1,19 @@
+--    Main interface through C to another process
+--    Copyright (C) 2016  Paul Coelho
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module Main where
@@ -39,11 +55,114 @@ foreign import ccall wrDoubles :: CInt -> CInt -> Ptr CDouble -> IO ()
 foreign import ccall exOpcode :: CInt -> CInt -> IO ()
 foreign import ccall hello :: CString -> IO CString
 
+data Opcode = Opcode {
+   readOp :: CInt,
+   writeOp :: CInt,
+   scriptOp :: CInt,
+   windowOp :: CInt,
+   fileOp :: CInt,
+   planeOp :: CInt,
+   confOp :: CInt,
+   boundariesOp :: CInt,
+   regionsOp :: CInt,
+   planesOp :: CInt,
+   sidesOp :: CInt,
+   sideOp :: CInt,
+   insidesOp :: CInt,
+   outsidesOp :: CInt,
+   insideOp :: CInt,
+   outsideOp :: CInt,
+   versorOp :: CInt,
+   vectorOp :: CInt,
+   delayOp :: CInt,
+   countOp :: CInt,
+   identOp :: CInt,
+   valueOp :: CInt,
+   -- scriptOp :: CInt,
+   topologyOp :: CInt,
+   fixedOp :: CInt,
+   pierceOp :: CInt,
+   seqnumOp :: CInt,
+   matrixOp :: CInt,
+   subconfOp :: CInt,
+   settingOp :: CInt,
+   textOp :: CInt,
+   opcodes :: CInt}
+
+data Configure = Configure {
+   testConf :: CInt}
+
 main = do
    print (holes 5 [2,3,4])
    setDebug (toCInt 0x10000)
    [rdfd,wrfd] <- fmap (map read) getArgs
-   testLoop rdfd wrfd
+   readOpV <- (newCString "ReadOp") >>= enumerate
+   writeOpV <- (newCString "WriteOp") >>= enumerate
+   scriptOpV <- (newCString "ScriptOp") >>= enumerate
+   windowOpV <- (newCString "WindowOp") >>= enumerate
+   fileOpV <- (newCString "FileOp") >>= enumerate
+   planeOpV <- (newCString "PlaneOp") >>= enumerate
+   confOpV <- (newCString "ConfOp") >>= enumerate
+   boundariesOpV <- (newCString "BoundariesOp") >>= enumerate
+   regionsOpV <- (newCString "RegionsOp") >>= enumerate
+   planesOpV <- (newCString "PlanesOp") >>= enumerate
+   sidesOpV <- (newCString "SidesOp") >>= enumerate
+   sideOpV <- (newCString "SideOp") >>= enumerate
+   insidesOpV <- (newCString "InsidesOp") >>= enumerate
+   outsidesOpV <- (newCString "OutsidesOp") >>= enumerate
+   insideOpV <- (newCString "InsideOp") >>= enumerate
+   outsideOpV <- (newCString "OutsideOp") >>= enumerate
+   versorOpV <- (newCString "VersorOp") >>= enumerate
+   vectorOpV <- (newCString "VectorOp") >>= enumerate
+   delayOpV <- (newCString "DelayOp") >>= enumerate
+   countOpV <- (newCString "CountOp") >>= enumerate
+   identOpV <- (newCString "IdentOp") >>= enumerate
+   valueOpV <- (newCString "ValueOp") >>= enumerate
+   -- scriptOpV <- (newCString "ScriptOp") >>= enumerate
+   topologyOpV <- (newCString "TopologyOp") >>= enumerate
+   fixedOpV <- (newCString "FixedOp") >>= enumerate
+   pierceOpV <- (newCString "PierceOp") >>= enumerate
+   seqnumOpV <- (newCString "SeqnumOp") >>= enumerate
+   matrixOpV <- (newCString "MatrixOp") >>= enumerate
+   subconfOpV <- (newCString "SubconfOp") >>= enumerate
+   settingOpV <- (newCString "SettingOp") >>= enumerate
+   textOpV <- (newCString "TextOp") >>= enumerate
+   opcodesV <- (newCString "Opcodes") >>= enumerate
+   testConfV <- (newCString "TestConf") >>= enumerate
+   mainLoop rdfd wrfd Opcode {
+   readOp = readOpV,
+   writeOp = writeOpV,
+   scriptOp = scriptOpV,
+   windowOp = windowOpV,
+   fileOp = fileOpV,
+   planeOp = planeOpV,
+   confOp = confOpV,
+   boundariesOp = boundariesOpV,
+   regionsOp = regionsOpV,
+   planesOp = planesOpV,
+   sidesOp = sidesOpV,
+   sideOp = sideOpV,
+   insidesOp = insidesOpV,
+   outsidesOp = outsidesOpV,
+   insideOp = insideOpV,
+   outsideOp = outsideOpV,
+   versorOp = versorOpV,
+   vectorOp = vectorOpV,
+   delayOp = delayOpV,
+   countOp = countOpV,
+   identOp = identOpV,
+   valueOp = valueOpV,
+   -- scriptOp = scriptOpV,
+   topologyOp = topologyOpV,
+   fixedOp = fixedOpV,
+   pierceOp = pierceOpV,
+   seqnumOp = seqnumOpV,
+   matrixOp = matrixOpV,
+   subconfOp = subconfOpV,
+   settingOp = settingOpV,
+   textOp = textOpV,
+   opcodes = opcodesV} Configure {
+   testConf = testConfV}
 
 toInt :: Integral a => IO a -> IO Int
 toInt = fmap (fromInteger . toInteger)
@@ -51,26 +170,33 @@ toInt = fmap (fromInteger . toInteger)
 toCInt :: Integral a => a -> CInt
 toCInt a = fromInteger (toInteger a)
 
-testLoop :: CInt -> CInt -> IO ()
-testLoop rdfd wrfd = do
-   readOp <- (newCString "ReadOp") >>= enumerate
-   fileOp <- (newCString "FileOp") >>= enumerate
-   planeOp <- (newCString "PlaneOp") >>= enumerate
-   confOp <- (newCString "ConfOp") >>= enumerate
-   textOp <- (newCString "TextOp") >>= enumerate
-   exOpcode rdfd readOp
+mainLoop :: CInt -> CInt -> Opcode -> Configure -> IO ()
+mainLoop rdfd wrfd op co = do
+   src <- rdOpcode rdfd
+   mainIter rdfd wrfd op co src
+   mainLoop rdfd wrfd op co
+
+mainIter :: CInt -> CInt -> Opcode -> Configure -> CInt -> IO ()
+mainIter rdfd wrfd op co src
+   | src == (readOp op) || src == (scriptOp op) = do
    ptr <- rdPointer rdfd
-   exOpcode rdfd fileOp
+   exOpcode rdfd (fileOp op)
    file <- toInt (rdInt rdfd)
-   exOpcode rdfd planeOp
+   exOpcode rdfd (planeOp op)
    plane <- toInt (rdInt rdfd)
-   exOpcode rdfd confOp
-   conf <- toInt (rdConfigure rdfd)
-   exOpcode rdfd textOp
+   exOpcode rdfd (confOp op)
+   conf <- rdConfigure rdfd
+   dataIter rdfd wrfd op co conf
+   wrOpcode wrfd src
+   wrPointer wrfd ptr
+mainIter _ _ _ _ _ = undefined
+
+dataIter :: CInt -> CInt -> Opcode -> Configure -> CInt -> IO ()
+dataIter rdfd wrfd op co conf
+   | conf == (testConf co) = do
+   exOpcode rdfd (textOp op)
    count <- toInt (rdInt rdfd)
    str <- newCString (replicate count '\0')
    rdChars rdfd (toCInt count) str
    (peekCString str) >>= print
-   wrOpcode wrfd readOp
-   wrPointer wrfd ptr
-   testLoop rdfd wrfd
+dataIter _ _ _ _ _ = undefined
--- haskell.c
+++ haskell.c
@@ -25,11 +25,16 @@
 
 static int debug = DEBUG;
 
-void error(const char *str, int wrt, const char *file, int line)
+void fatal(const char *str, int wrt, const char *file, int line)
 {
-	if (debug & 0x10000) exit(0);
     fprintf(stderr,"error: %s; wrt %d; file %s; line %d\n",str,wrt,file,line);
     exit(-1);
+}
+
+void error(const char *str, int wrt, const char *file, int line)
+{
+	if (debug & 0x10000) {printf("haskell done\n"); exit(0);}
+	fatal(str,wrt,file,line);
 }
 
 void setDebug(int val)
@@ -77,7 +82,7 @@ const char *enamerate(enum Opcode opcode)
 	case (SubconfOp): return "SubconfOp";
 	case (SettingOp): return "SettingOp";
 	case (TextOp): return "TextOp";
-	default: error("unknown opcode",opcode,__FILE__,__LINE__);}
+	default: fatal("unknown opcode",opcode,__FILE__,__LINE__);}
 	return 0;
 }
 
@@ -90,7 +95,6 @@ int enumerate(char *name)
 	if (strcmp(name,"FileOp") == 0) return FileOp;
 	if (strcmp(name,"PlaneOp") == 0) return PlaneOp;
 	if (strcmp(name,"ConfOp") == 0) return ConfOp;
-	if (strcmp(name,"Opcodes") == 0) return Opcodes;
 	if (strcmp(name,"BoundariesOp") == 0) return BoundariesOp;
 	if (strcmp(name,"RegionsOp") == 0) return RegionsOp;
 	if (strcmp(name,"PlanesOp") == 0) return PlanesOp;
@@ -115,7 +119,9 @@ int enumerate(char *name)
 	if (strcmp(name,"SubconfOp") == 0) return SubconfOp;
 	if (strcmp(name,"SettingOp") == 0) return SettingOp;
 	if (strcmp(name,"TextOp") == 0) return TextOp;
-	error("unknown opcode",0,__FILE__,__LINE__);
+	if (strcmp(name,"Opcodes") == 0) return Opcodes;
+	if (strcmp(name,"TestConf") == 0) return TestConf;
+	fatal("unknown name",0,__FILE__,__LINE__);
 	return -1;
 }
 
@@ -218,90 +224,90 @@ void rdDoubles(int fd, int count, double *doubles)
 void wrPointer(int fd, void *ptr)
 {
 	if (debug & 0x2) printf("wrPointer %d %p\n",getpid(),ptr);
-	if (write(fd,&ptr,sizeof(ptr)) != sizeof(ptr)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,&ptr,sizeof(ptr)) != sizeof(ptr)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrOpcode(int fd, int val)
 {
 	enum Opcode opcode = val;
 	if (debug & 0x2) printf("wrOpcode: %d %s\n",getpid(),enamerate(opcode));
-	if (write(fd,&opcode,sizeof(opcode)) != sizeof(opcode)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,&opcode,sizeof(opcode)) != sizeof(opcode)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrConfigure(int fd, int val)
 {
 	enum Configure conf = val;
 	if (debug & 0x2) printf("wrConfigure %d %d\n",getpid(),val);
-	if (write(fd,&conf,sizeof(conf)) != sizeof(conf)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,&conf,sizeof(conf)) != sizeof(conf)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrTopologyMode(int fd, int val)
 {
 	if (debug & 0x2) printf("wrTopologyMode %d %d\n",getpid(),val);
 	enum TopologyMode topology = val;
-	if (write(fd,&topology,sizeof(topology)) != sizeof(topology)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,&topology,sizeof(topology)) != sizeof(topology)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrFixedMode(int fd, int val)
 {
 	if (debug & 0x2) printf("wrFixedMode %d %d\n",getpid(),val);
 	enum FixedMode fixed = val;
-	if (write(fd,&fixed,sizeof(fixed)) != sizeof(fixed)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,&fixed,sizeof(fixed)) != sizeof(fixed)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrSubconf(int fd, int val)
 {
 	if (debug & 0x2) printf("wrSubconf %d %d\n",getpid(),val);
 	enum Subconf subconf = val;
-	if (write(fd,&subconf,sizeof(subconf)) != sizeof(subconf)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,&subconf,sizeof(subconf)) != sizeof(subconf)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrInt(int fd, int val)
 {
 	if (debug & 0x2) printf("wrInt %d %d\n",getpid(),val);
-	if (write(fd,&val,sizeof(val)) != sizeof(val)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,&val,sizeof(val)) != sizeof(val)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrFloat(int fd, float val)
 {
 	if (debug & 0x2) printf("wrFloat %d %f\n",getpid(),val);
-	if (write(fd,&val,sizeof(val)) != sizeof(val)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,&val,sizeof(val)) != sizeof(val)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrDouble(int fd, double val)
 {
 	if (debug & 0x2) printf("wrDouble %d %f\n",getpid(),val);
-	if (write(fd,&val,sizeof(val)) != sizeof(val)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,&val,sizeof(val)) != sizeof(val)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrChars(int fd, int count, char *chars)
 {
 	if (debug & 0x2) printf("wrChars %d %p\n",getpid(),chars);
-	if (write(fd,chars,count*sizeof(*chars)) != count*sizeof(*chars)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,chars,count*sizeof(*chars)) != count*sizeof(*chars)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrInts(int fd, int count, int *ints)
 {
 	if (debug & 0x2) printf("wrInts %d %p\n",getpid(),ints);
-	if (write(fd,ints,count*sizeof(*ints)) != count*sizeof(*ints)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,ints,count*sizeof(*ints)) != count*sizeof(*ints)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrFloats(int fd, int count, float *floats)
 {
 	if (debug & 0x2) printf("wrFloats %d %p\n",getpid(),floats);
-	if (write(fd,floats,count*sizeof(*floats)) != count*sizeof(*floats)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,floats,count*sizeof(*floats)) != count*sizeof(*floats)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void wrDoubles(int fd, int count, double *doubles)
 {
 	if (debug & 0x2) printf("wrDoubles %d %p\n",getpid(),doubles);
-	if (write(fd,doubles,count*sizeof(*doubles)) != count*sizeof(*doubles)) error("write failed",errno,__FILE__,__LINE__);
+	if (write(fd,doubles,count*sizeof(*doubles)) != count*sizeof(*doubles)) fatal("write failed",errno,__FILE__,__LINE__);
 }
 
 void exOpcode(int fd, int opcode)
 {
 	if (debug & 0x2) printf("exOpcode %d %s\n",getpid(),enamerate(opcode));
-	if (rdOpcode(fd) != opcode) error("unexpected opcode",errno,__FILE__,__LINE__);
+	if (rdOpcode(fd) != opcode) fatal("unexpected opcode",errno,__FILE__,__LINE__);
 }
 
 char *hello(char *hello)
